004b79b75bafc69ce3483715a0a3c350a3ed0bfa
Lucca Yuki <kelcoolg@gmail.com>
2026-03-18 08:25:27 -0300

    Ajustando o Menu

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,8 +1,10 @@
 import { Request, Response } from 'express'
+import { createMenuObject } from '../helpers/createMenuObject'
 
 export const home = (req: Request, res: Response) => {
     //res.send('home')
     res.render('pages/page', {
+        menu: createMenuObject('all'),
         banner: {
             title: 'Todos os animais',
             background: 'allanimals.jpg'
@@ -13,6 +15,7 @@ export const home = (req: Request, res: Response) => {
 export const dogs = (req: Request, res: Response) => {
     // res.send('dogs')
     res.render('pages/page', {
+        menu: createMenuObject('dogs'),
         banner: {
             title: 'Cachorros',
             background: 'banner_dog.jpg'
@@ -23,6 +26,7 @@ export const dogs = (req: Request, res: Response) => {
 export const cats = (req: Request, res: Response) => {
     // res.send('cats')
     res.render('pages/page', {
+        menu: createMenuObject('cats'),
         banner: {
             title: 'Gatos',
             background: 'banner_cat.jpg'
@@ -33,6 +37,7 @@ export const cats = (req: Request, res: Response) => {
 export const fishes = (req: Request, res: Response) => {
     // res.send('fishes')
     res.render('pages/page', {
+        menu: createMenuObject('fishes'),
         banner: {
             title: 'Peixes',
             background: 'banner_fish.jpg'
